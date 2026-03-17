5ab555748df74eb378a02b539f28e1607f346352
mzeric <eric@mezzofy.com>
2026-03-17 21:02:46 +0800

    Patch News/Blogs Pages

--- dist/i18n/i18n.js
+++ dist/i18n/i18n.js
@@ -163,7 +163,7 @@ class I18n {
         value = value[k];
       } else {
         console.warn(`Translation key not found: ${key}`);
-        return key; // Return key as fallback
+        return null; // Return null so HTML fallback content is preserved
       }
     }
 
